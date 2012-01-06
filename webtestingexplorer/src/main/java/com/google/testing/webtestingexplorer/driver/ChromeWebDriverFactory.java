9aa0f8ce16f322ac3a853204fb0209f0c2e62436
scott.d.mcmaster <scott.d.mcmaster@gmail.com>
2012-01-06 23:42:58 +0000

    Forgot to add the drivers last time

---
+++ webtestingexplorer/src/main/java/com/google/testing/webtestingexplorer/driver/ChromeWebDriverFactory.java
@@ -0,0 +1,36 @@
+/*
+Copyright 2011 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package com.google.testing.webtestingexplorer.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Creates {@link ChromeDriver}s.
+ * 
+ * @author scott.d.mcmaster@gmail.com (Scott McMaster)
+ */
+public class ChromeWebDriverFactory implements WebDriverFactory {
+
+  @Override
+  public WebDriver createWebDriver(WebDriverProxy proxy) throws Exception {
+    DesiredCapabilities driverCapabilities = new DesiredCapabilities();
+    driverCapabilities.setCapability(CapabilityType.PROXY, proxy.getSeleniumProxy());
+    return new ChromeDriver(driverCapabilities);
+  }
+}
---
+++ webtestingexplorer/src/main/java/com/google/testing/webtestingexplorer/driver/FirefoxWebDriverFactory.java
@@ -0,0 +1,62 @@
+/*
+Copyright 2011 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package com.google.testing.webtestingexplorer.driver;
+
+import net.jsourcerer.webdriver.jserrorcollector.JavaScriptError;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Creates {@link FirefoxDriver}s.
+ * 
+ * @author scott.d.mcmaster@gmail.com (Scott McMaster)
+ */
+public class FirefoxWebDriverFactory implements WebDriverFactory {
+
+  @Override
+  public WebDriver createWebDriver(WebDriverProxy proxy) throws Exception {
+    DesiredCapabilities driverCapabilities = new DesiredCapabilities();
+    driverCapabilities.setCapability(CapabilityType.PROXY, proxy.getSeleniumProxy());
+    
+    // Use a custom profile that trusts the cybervillians cert
+    // (to use Selenium with the BrowserMob proxy).
+    ProfilesIni allProfiles = new ProfilesIni();
+    System.setProperty("webdriver.firefox.profile","webtestingexplorer");
+    String browserProfile = System.getProperty("webdriver.firefox.profile");
+    FirefoxProfile profile = allProfiles.getProfile(browserProfile); 
+    profile.setAcceptUntrustedCertificates(true);
+    profile.setAssumeUntrustedCertificateIssuer(false);
+
+    // Install JSErrorCollector for JSErrorCollectorOracle to use if installed.
+    JavaScriptError.addExtension(profile);
+    profile.setProxyPreferences(proxy.getSeleniumProxy());
+    
+    // The following preferences control Firefox's default behavior of sending
+    // requests for favicon.ico, which results in lots of bogus 404's for sites
+    // that don't have favicons. If people want to use the tool to specifically
+    // look for 404's AND they have favicons, perhaps we should make this
+    // configurable.
+    profile.setPreference("browser.chrome.favicons", false);
+    profile.setPreference("browser.chrome.site_icons", false);
+    
+    return new FirefoxDriver(profile);
+  }
+}
