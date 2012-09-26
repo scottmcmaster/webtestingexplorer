c25c31a564ceb491e0514b71d5adb4b0a7713cb4
scott.d.mcmaster <scott.d.mcmaster@gmail.com>
2012-09-26 04:45:06 +0000

    Start a GWT test application where we can try new webtestingexplorer features in a controlled environment.

---
+++ webtestingexplorer/src/WebTestingExplorerGwtTestApp/src/org/webtestingexplorer/gwttestapp/server/GreetingServiceImpl.java
@@ -0,0 +1,54 @@
+package org.webtestingexplorer.gwttestapp.server;
+
+import javax.servlet.http.HttpSession;
+
+import org.webtestingexplorer.gwttestapp.client.GreetingService;
+import org.webtestingexplorer.gwttestapp.shared.FieldVerifier;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends RemoteServiceServlet implements
+    GreetingService {
+
+  public String greetServer(String input) throws IllegalArgumentException {
+    // Verify that the input is valid. 
+    if (!FieldVerifier.isValidName(input)) {
+      // If the input is not valid, throw an IllegalArgumentException back to
+      // the client.
+      throw new IllegalArgumentException(
+          "Name must be at least 4 characters long");
+    }
+
+    String serverInfo = getServletContext().getServerInfo();
+    String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+
+    // For trying webtestingexplorer against a stateful session servlet.
+    HttpSession session = getThreadLocalRequest().getSession();
+    session.setAttribute("name", input);
+
+    // Escape data from the client to avoid cross-site script vulnerabilities.
+    input = escapeHtml(input);
+    userAgent = escapeHtml(userAgent);
+
+    return "Hello, " + input + "!<br><br>I am running " + serverInfo
+        + ".<br><br>It looks like you are using:<br>" + userAgent;
+  }
+
+  /**
+   * Escape an html string. Escaping data received from the client helps to
+   * prevent cross-site script vulnerabilities.
+   * 
+   * @param html the html string to escape
+   * @return the escaped string
+   */
+  private String escapeHtml(String html) {
+    if (html == null) {
+      return null;
+    }
+    return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;");
+  }
+}
---
+++ webtestingexplorer/src/WebTestingExplorerGwtTestApp/src/org/webtestingexplorer/gwttestapp/server/StateServlet.java
@@ -0,0 +1,54 @@
+package org.webtestingexplorer.gwttestapp.server;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * An example of a servlet that exposes session state as a JSONObject,
+ * suitable for use with webtestingexplorer's JSONObjectState.
+ * Note that to get this to work properly, the XHR to retrieve the
+ * state needs to be made from the context of the browser running the
+ * application-under-test. This can be done via a call to
+ * WebDriver.executeScript(), and the result passed by the state checker
+ * to a new JSONObjectState instance.
+ * 
+ * NOTE ALSO that you would not want to expose an endpoint like this
+ * in a production web application.
+ * 
+ * @author scott.d.mcmaster@gmail.com (Scott McMaster)
+ */
+public class StateServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 6436886362958341997L;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    JSONObject state = new JSONObject();
+    @SuppressWarnings("unchecked")
+    Enumeration<String> namesEnum = request.getSession().getAttributeNames();
+    while (namesEnum.hasMoreElements()) {
+      String name = namesEnum.nextElement();
+      try {
+        state.put(name, request.getSession().getAttribute(name));
+      } catch (JSONException e) {
+        throw new ServletException(e);
+      }
+    }
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    try {
+      state.write(response.getWriter());
+    } catch (JSONException e) {
+      throw new ServletException(e);
+    }
+  }
+}
